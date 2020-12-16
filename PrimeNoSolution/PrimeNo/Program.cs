c23238b8722635492f54cb204458f68fb7ffee30
unknown <pranav.adiraju@lntinfotech.com>
2020-12-16 16:47:48 +0530

    1st Commit

---
+++ PrimeNoSolution/PrimeNo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lower, upper, i, j, flag;
+            Console.WriteLine("Enter lower bound of " +
+                      "the interval: ");
+            lower = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter upper bound of " +
+                     "the interval: ");
+            upper = int.Parse(Console.ReadLine());
+
+            for (i = lower; i <= upper; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
